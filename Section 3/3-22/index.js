0f5159935d3061bf1ba118ee78a330919bd4f02a
Collin Stewart <collin.stewart30@yahoo.com>
2022-12-06 12:19:48 -0500

    Section 3 - Lesson 22 completed

---
+++ Section 3/3-22/index.js
@@ -0,0 +1,19 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello 
+          to="Ringo"
+          from="Paul"
+          bangs={4}
+        />
+        <Hello 
+          to="George"
+        />
+      </div>
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
