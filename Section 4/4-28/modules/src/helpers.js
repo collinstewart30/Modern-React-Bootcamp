a8b0cc4c2e1c22ad9e2215c28c0db664f3a2a174
Collin Stewart <collin.stewart30@yahoo.com>
2022-12-08 12:16:52 -0500

    Lesson 28 Completed

---
+++ Section 4/4-28/modules/src/helpers.js
@@ -0,0 +1,14 @@
+function helpful() {
+    console.log ('I did something helpful');
+}
+
+function sort() {
+    console.log("We sorted something");
+}
+
+function sing() {
+    console.log('LALALALA');
+}
+
+export default helpful;
+export {sort, sing};
