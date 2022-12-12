052a78dd7b91f49378090b030e78389729c2c748
Collin Stewart <collin.stewart30@yahoo.com>
2022-12-12 15:33:03 -0500

    Section 5 Lesson 37 done

--- Section 5 - Pokedex Project/pokemon/src/Pokecard.js
+++ Section 5 - Pokedex Project/pokemon/src/Pokecard.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import './Pokecard.css';
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+//const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 class Pokecard extends Component {
     render() {
         const props = this.props;
-        let imgSrc = `${POKE_API}${props.id}.png`
+        let n = props.id;
+        let imgID = String(n).padStart(3, '0');
+        let imgSrc = `${POKE_API}${imgID}.png`
         return (
             <div className="Pokecard">
                 <h2 className="Pokecard-title">{props.name}</h2>
